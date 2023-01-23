7ba887032c63c537bb4802145659b287b16aeb9d
TaoBi22 <beahealy22@gmail.com>
2023-01-23 16:27:21 +0000

    Add circt-lec to config tools

--- integration_test/lit.cfg.py
+++ integration_test/lit.cfg.py
@@ -80,7 +80,8 @@ tool_dirs = [
 ]
 tools = [
     'circt-opt', 'circt-translate', 'firtool', 'circt-rtl-sim.py',
-    'esi-cosim-runner.py', 'equiv-rtl.sh', 'handshake-runner', 'hlstool'
+    'esi-cosim-runner.py', 'equiv-rtl.sh', 'handshake-runner', 'hlstool',
+    'circt-lec'
 ]
 
 # Enable python if its path was configured
