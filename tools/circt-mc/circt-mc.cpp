7f99ae99d64e7fd081a45808f308abe269265ca7
TaoBi22 <beahealy22@gmail.com>
2023-02-10 18:33:09 +0000

    Remove some circt-lec specific documentation

--- tools/circt-mc/circt-mc.cpp
+++ tools/circt-mc/circt-mc.cpp
@@ -29,7 +29,7 @@ namespace cl = llvm::cl;
 // Command-line options declaration
 //===----------------------------------------------------------------------===//
 
-static cl::OptionCategory mainCategory("circt-lec Options");
+static cl::OptionCategory mainCategory("circt-mc Options");
 
 static cl::opt<std::string>
     moduleName1("c1",
@@ -83,11 +83,10 @@ int main(int argc, char **argv) {
   mlir::registerMLIRContextCLOptions();
 
   // Parse the command-line options provided by the user.
-  cl::ParseCommandLineOptions(
-      argc, argv,
-      "circt-lec - logical equivalence checker\n\n"
-      "\tThis tool compares two input circuit descriptions to determine whether"
-      " they are logically equivalent.\n");
+  cl::ParseCommandLineOptions(argc, argv,
+                              "circt-mc - bounded model checker\n\n"
+                              "\tThis tool checks that properties hold in a "
+                              "design over a symbolic bounded execution.\n");
 
   // Set the bug report message to indicate users should file issues on
   // llvm/circt and not llvm/llvm-project.
