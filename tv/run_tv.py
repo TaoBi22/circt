4a9cdea8b372f7a72d101e92742217929f5afc8b
Bea Healy <beahealy22@gmail.com>
2025-06-11 17:43:50 +0100

    Add SSA name collection

--- tv/run_tv.py
+++ tv/run_tv.py
@@ -11,9 +11,13 @@ if len(sys.argv) < 2:
 fsmFile = sys.argv[1]
 
 moduleName = ""
+# Fetch the widths of our various values for signatures etc., and their SSA names for comparison
 inputWidths = []
+inputNames = []
 outputWidths = []
+outputNames = []
 varWidths = []
+varNames = []
 timeWidth = 32
 with open(fsmFile, "r") as f:
     x = ""
@@ -25,6 +29,8 @@ with open(fsmFile, "r") as f:
     for input in inputs.split(","):
         if width := re.search(r": i([0-9]+)", input):
             inputWidths.append(int(width.group(1)))
+        if name := re.search(r"%([\s\S]*) :", input):
+            inputNames.append(int(width.group(1)))
     outputs = search.group(3)
     for output in outputs.split(","):
         if width := re.search(r"i([0-9]+)", output):
@@ -33,9 +39,13 @@ with open(fsmFile, "r") as f:
     x = ""
     while not "fsm.state" in x:
         x = f.readline()
-        if search := re.search(r"%[\s\S]+ = fsm.variable [\s\S]+ : i([0-9]+)", x):
-            varWidths.append(search.group(1))
+        if search := re.search(r"%([\s\S]+) = fsm.variable [\s\S]+ : i([0-9]+)", x):
+            varNames.append(search.group(1))
+            varWidths.append(search.group(2))
 
+assert len(inputWidths) == len(inputNames), "Input widths and names do not match"
+assert len(outputWidths) == len(outputNames), "Output widths and names do not match"
+assert len(varWidths) == len(varNames), "Variable widths and names do not match"
 
 print(inputWidths)
 print(moduleName)
@@ -61,6 +71,16 @@ with open(f"{builddir}/untimed_rtl.mlir") as file:
     text += ["}"]*2
     with open(f"{builddir}/rtl.mlir", "w+") as newFile:
         newFile.writelines(text)
+
+    # Since we have this text knocking around anyway, we might as well grab the output SSA names
+    # First check we actually have outputs
+    if terminator.strip() != "hw.output":
+        x = re.search("hw.output[\s]?(%[A-Za-z0-9_])?(, %[A-Za-z0-9_])+ :", terminator)
+        outputNames.append(x.group(0))
+        if x.group(1):
+            outputNames = [x.group(1).split(",")]
+
+print("Output names:", outputNames)
 
 os.system(f"../build/bin/circt-opt --externalize-registers --lower-to-bmc=\"top-module=fsm10 bound=10\" --convert-hw-to-smt --convert-comb-to-smt --convert-verif-to-smt --canonicalize {builddir}/rtl.mlir > {builddir}/bmc.mlir")
 
@@ -106,6 +126,16 @@ for i, invariant in enumerate(invariants):
     # Make sure that the times match
     propertyStr += f"%rightTime = smt.eq %rtlTime, %time_reg : !smt.bv<32>\n"
     propertyStr += f"%precondition = smt.and %apply, %rightTime : !smt.bool\n"
+
+    # Check equivalence of variables:
+    inputChecks = []
+    for j, varName in enumerate(varNames):
+        propertyStr += f"%var_{j}_eq = smt.eq %var_{j}, %{varName} : !smt.bv<{varWidths[j]}>\n"
+        inputChecks.append(f"%var_{j}_eq")
+
+    # Check equivalence of outputs:
+    # First, we need to get the SSA symbols that correspond to the outputs, since this is not something we can extract from the signature
+
     # TODO handle input equivalence
     propertyStr += "}\n"
     propertyStr += f"smt.assert %tvclause_{i}"
