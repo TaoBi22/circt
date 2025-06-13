2210c26a7c46dc838cd1ec35f95d62adef55b178
Bea Healy <beahealy22@gmail.com>
2025-06-13 11:59:41 +0100

    Lots and lots of debugging and a bit of sleep later..... i think its working.... god bless america......

--- tv/testing/run.sh
+++ tv/testing/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-circt-opt --lower-smt-to-z3-llvm tv.mlir --reconcile-unrealized-casts > exec.mlir
-mlir-cpu-runner exec.mlir -e fsm10 -shared-libs=/usr/lib/libz3.so -entry-point-result=void
+../../build/bin/circt-opt --lower-smt-to-z3-llvm tv.mlir --reconcile-unrealized-casts > exec.mlir
+../../llvm/build/bin/mlir-cpu-runner exec.mlir -e fsm10 -shared-libs=/usr/lib/x86_64-linux-gnu/libz3.so -entry-point-result=void
