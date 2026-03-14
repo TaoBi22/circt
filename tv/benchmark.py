653fbc0f415c9279a89b516e34409cf5aad5657f
Bea Healy <beahealy22@gmail.com>
2026-03-14 20:14:39 +0000

    New results!

--- tv/benchmark.py
+++ tv/benchmark.py
@@ -7,17 +7,19 @@ def timed_system(cmd: str):
     end = time.perf_counter()
     return end - start
 
+reps = 10
 
 with open("benchmark_results_10.txt", "w") as resultsFile:
     resultsFile.write("states,time\n")
     for sCount in range(10, 60, 10):
-        t = timed_system(f"python3 run_tv.py benchmarks/fsm_{sCount}states_0loops.mlir 10")
-        resultsFile.write(f"{sCount}, {t}\n")
-sys.exit(0)
+        for i in range(reps):
+            t = timed_system(f"python3 run_tv.py benchmarks/fsm_{sCount}states_0loops.mlir 10")
+            resultsFile.write(f"{sCount}, {t}\n")
 
 with open("benchmark_results_50.txt", "w") as resultsFile:
     resultsFile.write("states,time\n")
     for sCount in range(10, 60, 10):
-        t = timed_system(f"python3 run_tv.py benchmarks/fsm_{sCount}states_0loops.mlir 50")
-        resultsFile.write(f"{sCount}, {t}\n")
+        for i in range(reps):
+            t = timed_system(f"python3 run_tv.py benchmarks/fsm_{sCount}states_0loops.mlir 50")
+            resultsFile.write(f"{sCount}, {t}\n")
 
