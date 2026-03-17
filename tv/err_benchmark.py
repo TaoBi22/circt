671042fb05e584f9b476fd0fdcb995d1ca0c9a7e
Bea Healy <beahealy22@gmail.com>
2026-03-17 19:51:21 +0000

    PRepare for err benchmarking

---
+++ tv/err_benchmark.py
@@ -0,0 +1,27 @@
+import time, os
+import sys
+
+def timed_system(cmd: str):
+    start = time.perf_counter()
+    os.system(cmd)
+    end = time.perf_counter()
+    return end - start
+
+reps = 3
+
+with open("err_benchmark_results_10.txt", "w") as resultsFile:
+    resultsFile.write("states,time\n")
+    for sCount in range(10, 60, 10):
+        for i in range(reps):
+            print(f"rep {i}, scount {sCount}")
+            t = timed_system(f"python3 run_tv.py err_benchmarks/fsm_{sCount}states_0loops.mlir 10")
+            resultsFile.write(f"{sCount}, {t}\n")
+
+with open("err_benchmark_results_50.txt", "w") as resultsFile:
+    resultsFile.write("states,time\n")
+    for sCount in range(10, 60, 10):
+        for i in range(reps):
+            print(f"rep {i}, scount {sCount}")
+            t = timed_system(f"python3 run_tv.py benchmarks/fsm_{sCount}states_0loops.mlir 50")
+            resultsFile.write(f"{sCount}, {t}\n")
+
