4e04588012c6c2cd3d1efb184e86eb4a05c5a4c9
Bea Healy <beahealy22@gmail.com>
2026-07-06 15:54:34 +0200

    [axi4] Add AXI4PortMappingAttrInterface

---
+++ lib/Dialect/AXI4/AXI4Interfaces.cpp
@@ -0,0 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the AXI4 attr interfaces.
+//
+//===----------------------------------------------------------------------===//
+
+#include "circt/Dialect/AXI4/AXI4Interfaces.h"
+
+//===----------------------------------------------------------------------===//
+// TableGen generated logic.
+//===----------------------------------------------------------------------===//
+
+#include "circt/Dialect/AXI4/AXI4AttrInterfaces.cpp.inc"
